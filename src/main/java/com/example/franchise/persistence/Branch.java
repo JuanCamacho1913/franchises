d0a97fcea7a66fad7a8eb60a91cdfaa55608064b
¨juan <jecb1913@gmail.com>
2024-10-07 19:33:56 -0500

    Create entities :rocket:

--- src/main/java/com/example/franchise/persistence/Branch.java
+++ src/main/java/com/example/franchise/persistence/Branch.java
@@ -1,2 +1,22 @@
-package com.example.franchise.persistence;public class Branch {
+package com.example.franchise.persistence;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "sucursal")
+public class Branch {
+
+    @Id
+    private String id;
+    private String name;
+    private List<Product> products;
 }
--- src/main/java/com/example/franchise/persistence/Product.java
+++ src/main/java/com/example/franchise/persistence/Product.java
@@ -1,2 +1,20 @@
-package com.example.franchise.persistence;public class Product {
+package com.example.franchise.persistence;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "producto")
+public class Product {
+
+    @Id
+    private String id;
+    private String name;
+    private String stock;
 }
